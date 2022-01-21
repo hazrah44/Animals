2b35856ebc350455982e8c257d1fa7006c98efc5
hazrahothman <77723723+hazrahothman@users.noreply.github.com>
2022-01-21 01:34:46 -0500

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -1,20 +1,25 @@
-package main;
-
-public abstract class Animal {
-
-private int hunger;
+package Animals;
+//MET CS 622_@Assignment 1_Hazrah Othman
+//1.6.2 Code showing an abstract class or interface
+//Show the relevant code (only) and explain why an abstract class or interface is appropriate here. It should be clear where the code is located (class and method).
+//This is animals class
+public abstract class Animals {
 	
-	public Animal() {
+	private  int hunger;
+	
+	public Animals() {
 		hunger = 0;
-	
+	}
 	public int getHunger() {
-	
+		return hunger;
+	}
 	public abstract void talk();
-	
-	
-	public void timePasses()
-	
+	public void timePasses() {
+		hunger++;
+	}
 	public void food() {
+		hunger = 0;
+	}
+	public abstract String toString();
 	
-	public abstract String toString();	
-}
+	}
