1eb4ad3987a1fba34e9d96ef2d991f2e471f453f
Hazrah Othman <hazrah44>
2022-01-26 14:05:41 -0500

    First package Commit

---
+++ Animals/src/com/animal/Animals.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.animal;
+
+/**
+ * @author hazrahothman
+ *
+ */
+public class Animals {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
