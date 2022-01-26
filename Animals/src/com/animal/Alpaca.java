23906b60a0f36fa60ffd2d91ded3222eda9e1bc8
Hazrah Othman <hazrah44>
2022-01-26 14:15:13 -0500

    Alpaca Commit

--- Animals/src/com/animal/Alpaca.java
+++ Animals/src/com/animal/Alpaca.java
@@ -7,14 +7,20 @@ package com.animal;
  * @author hazrahothman
  *
  */
-public class Alpaca {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+//MET CS 622_@Assignment 1_Hazrah Othman
+//1.6.4 Code showing upcasting or downcasting
+//Show the relevant code (only) and explain why upcasting or downcasting is appropriate here. It should be clear where the code is located (class and method).
+//Inheritance 
+//superclass animals for alpaca
+public class Alpaca extends Animals {
+	public Alpaca() {
+		super();
 	}
-
+	public void talk() {
+		//Reference: https://en.wikipedia.org/wiki/List_of_animal_sounds
+		System.out.println("Cluck");
+	}
+	public String toString() {
+		return "Alpaca";
+	}
 }
