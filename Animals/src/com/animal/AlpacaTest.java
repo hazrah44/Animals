5436321ec021473424eb5dd3469e821193fae60f
Hazrah Othman <hazrah44>
2022-01-27 00:32:11 -0500

    Commit

---
+++ Animals/src/com/animal/AlpacaTest.java
@@ -0,0 +1,19 @@
+package com.animal;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.jar.Attributes.Name;
+
+import org.junit.jupiter.api.Test;
+
+class AlpacaTest {
+
+	@SuppressWarnings("unused")
+	@Test
+	void test() {
+		Short output;
+		equals (Name.SIGNATURE_VERSION);
+	}
+
+}
---
+++ Animals/src/com/animal/AnimalsTest.java
@@ -0,0 +1,19 @@
+package com.animal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.jar.Attributes.Name;
+
+import org.junit.jupiter.api.Test;
+
+class AnimalsTest {
+
+	@SuppressWarnings("unused")
+	@Test
+	void test() {
+		Short output;
+		equals (Name.SIGNATURE_VERSION);
+	}
+	}
+
+
